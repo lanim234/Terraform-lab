1b994decf4c61a8eea2b1a0316dac9d7c7a580a3
lanim234 <niyi_meg@yahoo.com>
2023-08-22 16:32:32 -0500

    hbeu

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,9 +26,9 @@ resource "aws_instance" "web" {
 
 
 data "aws_ami" "lake" {
-  owners      = ["973714476881"]
+  owners      = ["617766776617"]
   most_recent = true
-  name_regex  = "Centos-8-Devops-Practice"
+  name_regex  = "devops-practice"
 }
 
 resource "aws_security_group" "sg" {
