65adae79fa5a0441fdd66da2754bdf8150faca22
lanim234 <niyi_meg@yahoo.com>
2023-08-22 15:56:52 -0500

    hbeu

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 module "instances" {
   for_each = var.instances
   source = "./ec2"
