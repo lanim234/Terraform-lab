a489654064919a19e363e1b2683f0324f8d2f975
Lanim <semeboerch@gmail.com>
2023-08-23 18:59:34 -0500

    hi

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,57 +1,17 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.lake.id
-  instance_type = "t3.small"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  tags = {
-    Name = var.name
-  }
-
-  provisioner "remote-exec" {
-
-    connection {
-      type = "ssh"
-      user = "centos"
-      password = "DevOps321"
-      host = aws_instance.web.public_ip
-    }
-
-    inline = [
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -u https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
-
-    ]
-  }
+resource "aws_ssm_parameter" "man" {
+  count = length(var.man)
+  name  = var.man[count.index].name
+  type  = var.man[count.index].type
+  value = var.man[count.index].value
 }
 
 
-data "aws_ami" "lake" {
-  owners      = ["617766776617"]
-  most_recent = true
-  name_regex  = "devops-practice"
+
+
+variable "man" {
+  default = [
+    {name = "demo1", value = "demo2", "demo3" }
+    {name = "demo2", value = "demo2", "demo3"}
+    {name = "demo3", value = "demo3", "demo3"}
+  ]
 }
-
-resource "aws_security_group" "sg" {
-  name        = var.name
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.name
-  }
-}
-
-variable "name" {}
--- terra-practice/main.tf
+++
@@ -1,7 +0,0 @@
-variable "letn" {
-        default = 100
-}
-
-output "letn" {
-        value = var.letn >90 ? "surplus quantity" : "less quantity"
-}
