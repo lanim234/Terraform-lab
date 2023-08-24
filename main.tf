c6009961c5567b9391958b8d01ac854a6540f8df
Lanim <semeboerch@gmail.com>
2023-08-23 19:47:39 -0500

    hi

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_ssm_parameter" "man" {
-  count = length(var.man)
-  name  = var.man[count.index].name
-  value = var.man[count.index].value
-  type = var.man[count.index].type
+resource "aws_ssm_parameter" "mans" {
+  count = length(var.mans)
+  name  = var.mans[count.index].name
+  value = var.mans[count.index].value
+  type = var.mans[count.index].type
 }
 
 
 
 
-variable "man" {
+variable "mans" {
   default = [
     {name = "demo1", value = "demo1", type="string" },
     {name = "demo2", value = "demo2", type="string" }
