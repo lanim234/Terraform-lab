db16abdb018b4c72ef2329455a0eb6116268f59b
Lanim <semeboerch@gmail.com>
2023-08-23 19:21:26 -0500

    hi

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_ssm_parameter" "man" {
 
 variable "man" {
   default = [
-    {name = "demo1", value = "demo1", type = "string" },
-    {name = "demo2", value = "demo2", type =  "string" }
+    {name = "demo1", value = "demo1", type="string" },
+    {name = "demo2", value = "demo2", type="string" }
   ]
 }
