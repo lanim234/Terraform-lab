df3b4243d6705c23d44d2f28767ef58fba5d5e8f
Lanim <semeboerch@gmail.com>
2023-09-01 05:56:24 -0500

    hi

---
+++ Terraform-vpc-project/main.tf
@@ -0,0 +1,15 @@
+module "autoscaling" {
+  source = "./modules/autoscaling"
+  namespace = var.namespace
+}
+
+module "database" {
+  source = "./modules/database"
+  namespace = var.namespace
+}
+
+module "networking" {
+  source    = "./modules/networking"
+  namespace = var.namespace
+}
+
